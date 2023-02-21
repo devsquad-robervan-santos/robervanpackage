7830cfcc635bc0eb2962e67e8c4be2d046277edd
RobervanPires <robervanpires01@gmail.com>
2023-02-21 10:05:57 -0300

    remove test files

--- index.js
+++ index.js
@@ -1,43 +1,40 @@
-//doHttpPost('localhost', 8000, '/TestPost', 'string' , 'TestTestTestTest', false);
-//doHttpPost('localhost', 8000, '/TestPost', 'file' , '/Users/chengwei/Downloads/grid1.png', true);
 
-var querystring = require('querystring')
+const queryString = require('querystring');
+const http = require('http');
 
-function doHttpPost(_host, _port, _path, value) {
-	var http = require('http'),
-	    postData,
-        postOptions,
-        postRequest;
-
-    postData = value
-    postOptions = {
-        'host': _host,
-        'port': _port,
-        'path': _path,
-        'method': 'POST',
-        'headers' : {
-            'X-COCKPIT-TOKEN': '6d5c352b-98d3-4ccb-8bd6-1f192208cff8',
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Accept': 'application/json',
-            'Content-Length' : postData.length
-        }
-    };
-    postRequest = http.request(postOptions, function(response){
-        console.log('Status Code:', response.statusCode);
-
-        response.on('end', function() {
-            console.log('sent ' + value);
-        });
-    });
-    postRequest.write(postData);
-    postRequest.end();
-}
-
-var postData = querystring.stringify({
-    'exception': 'some-exception',
-    'message': 'some-message',
-    'file': 'some-file',
-    'type': 'cli',
+const data = queryString.stringify({
+    exception: 'test exception nodejs',
+    message: 'test message nodejs',
+    file: 'test-node.js',
+    type: 'cli',
 });
 
-doHttpPost('cockpit-app.test', 80, '/webhook', postData);
+const options = {
+    hostname: 'cockpit-app.test',
+    path: '/webhook',
+    port: 80,
+    method: 'POST',
+    headers: {
+        'X-COCKPIT-TOKEN': '6d5c352b-98d3-4ccb-8bd6-1f192208cff8',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json',
+    }
+};
+
+const req = http.request(options, (res) => {
+    console.log('Status Code:', res.statusCode);
+
+})
+
+req.on("error", (err) => {
+    console.log("Error: ", err.message);
+});
+
+req.write( data);
+req.end();
+
+/* function robervanpackage(){
+
+}
+
+module.exports.robervanpackage = robervanpackage; */
